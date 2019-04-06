a786bc5f93b5055c43659d7b16360f3ae2ac5a90
Andrew4d3 <andrew4d3@gmail.com>
2019-04-06 00:58:04 -0300

    Including missing files and changes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const MongoClient = require("mongodb").MongoClient;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const mongoUri = "mongodb://root:123456@db:27017";
+const dbName = "test";
+
+router.get("/", async function(req, res, next) {
+  const client = await MongoClient.connect(mongoUri);
+  const db = client.db(dbName);
+  const dbInfo = await db.command({ buildInfo: 1 });
+  const dbInfoJson = JSON.stringify(dbInfo, null, 4);
+
+  res.render("index", { title: "Express", dbInfo: dbInfoJson });
+  db.close();
 });
 
 module.exports = router;
