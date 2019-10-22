37afeeabdc0155418f41e11a34537f4e915250f0
LuisEnMarroquin <mluis651@gmail.com>
2019-10-22 16:03:11 -0500

    build: Latest lts-apline node image, my .vscode settings and standard.js

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const router = express.Router();
-const MongoClient = require("mongodb").MongoClient;
+const express = require('express')
+const router = express.Router()
+const MongoClient = require('mongodb').MongoClient
 
 /* GET home page. */
-const mongoUri = "mongodb://root:123456@db:27017";
-const dbName = "test";
+const mongoUri = 'mongodb://root:123456@db:27017'
+const dbName = 'test'
 
-router.get("/", async function(req, res, next) {
-  const client = await MongoClient.connect(mongoUri);
-  const db = client.db(dbName);
-  const dbInfo = await db.command({ buildInfo: 1 });
-  const dbInfoJson = JSON.stringify(dbInfo, null, 4);
+router.get('/', async function (req, res, next) {
+  const client = await MongoClient.connect(mongoUri)
+  const db = client.db(dbName)
+  const dbInfo = await db.command({ buildInfo: 1 })
+  const dbInfoJson = JSON.stringify(dbInfo, null, 4)
 
-  res.render("index", { title: "Express", dbInfo: dbInfoJson });
-  client.close();
-});
+  res.render('index', { title: 'Express', dbInfo: dbInfoJson })
+  client.close()
+})
 
-module.exports = router;
+module.exports = router
